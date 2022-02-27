e1a8301313139b4e029d1ad18f2b986a793509fc
Yago Ferreira <yferreira@hsolmkt.com.br>
2022-02-27 08:32:07 -0300

    Next Project

---
+++ components/Layout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './Header'
+
+const Layout = ({ title, keywords, description, children }) => {
+  return (
+    <div className=''>
+      <head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+      </head>
+      <Header />
+      <main className='container mx-auto my-7'>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: "DevSpace",
+  keywords: "Development, programming",
+  description: "The best info & news in development"
+}
+
+export default Layout
---
+++ pages/blog/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const BlogPage = () => {
+  return (
+    <Layout title={"Blog Page"}>
+      <h1 className="text-6xl text-gray-800 my-3">
+        Blog Page
+      </h1>
+    </Layout>
+  )
+}
+
+export default BlogPage
